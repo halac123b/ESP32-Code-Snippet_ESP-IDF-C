fa73b9215f55ff0ac6111812837608ccfe47b9e1
halac123b <halac123b@gmail.com>
2023-10-18 17:40:04 +0700

    Update 1

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -15,7 +15,27 @@
 #include <driver/gpio.h>
 
 
-#define BUTTON_PIN GPIO_NUM_0 //coi GPIO_NUM_May la button thi dung cai nay e Son dang xai chan 0
+#define TOUCH_GPIO_NUM 13
+#define RESET_BUTTON_GPIO_NUM 0
+
+void initialize_reset_button() {
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << RESET_BUTTON_GPIO_NUM);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE; // or GPIO_PULLDOWN_ENABLE if needed
+    gpio_config(&io_conf);
+}
+
+void monitor_reset_button(void* parameter) {
+    while(1) {
+        if (gpio_get_level(RESET_BUTTON_GPIO_NUM) == 0) {
+            printf("ESP32\n");
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
+
 
 void print_mssv(void* parameter) {
     while(1) {
@@ -24,25 +44,23 @@ void print_mssv(void* parameter) {
     }
 }
 
-void button(void* parameter) {
-    while(1) {
-        if(gpio_get_level(BUTTON_PIN) == 0) {
-            printf("ESP32\n");
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-}
+
+// void touch(void* parameter) {
+//     esp_rom_gpio_pad_select_gpio(TOUCH_GPIO_NUM);
+//     gpio_set_direction(TOUCH_GPIO_NUM, GPIO_MODE_INPUT);
+//     gpio_set_pull_mode(TOUCH_GPIO_NUM, GPIO_PULLUP_ONLY);
+//     while(1) {
+//         if (gpio_get_level(TOUCH_GPIO_NUM) == 0) {
+//             printf("ESP32\n");
+//         }
+//         vTaskDelay(100 / portTICK_PERIOD_MS);
+//     }
+// }
 
 void app_main() {
-    printf("Hello world!");
     xTaskCreate(print_mssv, "print_mssv", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    //config the GPIO pin for button
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.pin_bit_mask = 1ULL << BUTTON_PIN;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = GPIO_PULLUP_ONLY;
-    gpio_config(&io_conf);
-    xTaskCreate(button, "button", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    // xTaskCreate(touch, "touch", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    initialize_reset_button();
+    xTaskCreate(monitor_reset_button, "monitor_reset_button", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    
 }
